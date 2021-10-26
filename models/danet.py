f55e903c2208f5fd890f2a1046d8bcc0ca4d1462
Zhengyang Yu <JoanYu@users.noreply.github.com>
2021-10-26 19:03:29 +0800

    fix some mistakes

--- models/danet.py
+++ models/danet.py
@@ -1,6 +1,6 @@
-# DANet by xmu-xiaoma666
+# DANet by CASIA IVA
 # "Dual Attention Network for Scene Segmentation" arxiv: 1809.02983
-# WeChat Official Accounts: FightingCV
+# jliu@nlpr.ia.ac.cn
 
 import numpy as np
 import torch
@@ -8,192 +8,111 @@ from torch import nn
 from torch.nn import init
 
 class PAM(nn.Module):
+    # Ref from SAGAN
     '''
     Position Attention Module
     '''
 
-    def __init__(self,d_model=512,kernel_size=3,H=7,W=7):
-        super().__init__()
-        self.cnn=nn.Conv2d(d_model,d_model,kernel_size=kernel_size,padding=(kernel_size-1)//2)
-        self.pa=SAM(d_model,d_k=d_model,d_v=d_model,h=1)
-    
-    def forward(self,x):
-        bs,c,h,w=x.shape
-        y=self.cnn(x)
-        y=y.view(bs,c,-1).permute(0,2,1) #bs,h*w,c
-        y=self.pa(y,y,y) #bs,h*w,c
-        return y
-    
-class CAM(nn.Module):
-    def __init__(self,d_model=512,kernel_size=3,H=7,W=7):
-        super().__init__()
-        self.cnn=nn.Conv2d(d_model,d_model,kernel_size=kernel_size,padding=(kernel_size-1)//2)
-        self.pa=SimSAM(H*W,h=1)
-    
-    def forward(self,x):
-        bs,c,h,w=x.shape
-        y=self.cnn(x)
-        y=y.view(bs,c,-1) #bs,c,h*w
-        y=self.pa(y,y,y) #bs,c,h*w
-        return y
-    
-class SAM(nn.Module):
-    # Scaled Dot-Product Attention Module
-    # "Attention Is All You Need" arxiv: 1706.03762
+    def __init__(self,in_planes, ratio=8):
+        super(PAM, self).__init__()
+        self.in_planes = in_planes
+        self.key_conv=nn.Conv2d(in_planes, in_planes // ratio, kernel_size=1)
+        self.value_conv=nn.Conv2d(in_planes, in_planes ,kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
 
-    def __init__(self, d_model, d_k, d_v, h,dropout=.1):
-        '''
-        :param d_model: Output dimensionality of the model
-        :param d_k: Dimensionality of queries and keys
-        :param d_v: Dimensionality of values
-        :param h: Number of heads
-        '''
-        super(SAM, self).__init__()
-        self.fc_q = nn.Linear(d_model, h * d_k)
-        self.fc_k = nn.Linear(d_model, h * d_k)
-        self.fc_v = nn.Linear(d_model, h * d_v)
-        self.fc_o = nn.Linear(h * d_v, d_model)
-        self.dropout=nn.Dropout(dropout)
+    def forward(self, x):
+        """
+            x: B*C*H*W
+            q: B*(C/r)*(H*W)
+            v: B*(C/r)*(H*W)
+            attention: B*(H*W)*(H*W)
+            out: B*C*H*W
+        """
+        B, C, H, W = x.size()
+        query = self.key_conv(x).view(B, -1, H*W)
+        key = self.key_conv(x).view(B, -1, H*W)
+        attention = self.softmax(torch.bmm(query.permute(0, 2, 1), key))
+        value = self.value_conv(x).view(B, -1, H*W)
 
-        self.d_model = d_model
-        self.d_k = d_k
-        self.d_v = d_v
-        self.h = h
-
-        self.init_weights()
-
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, queries, keys, values, attention_mask=None, attention_weights=None):
-        '''
-        Computes
-        :param queries: Queries (b_s, nq, d_model)
-        :param keys: Keys (b_s, nk, d_model)
-        :param values: Values (b_s, nk, d_model)
-        :param attention_mask: Mask over attention values (b_s, h, nq, nk). True indicates masking.
-        :param attention_weights: Multiplicative weights for attention values (b_s, h, nq, nk).
-        :return:
-        '''
-        b_s, nq = queries.shape[:2]
-        nk = keys.shape[1]
-
-        q = self.fc_q(queries).view(b_s, nq, self.h, self.d_k).permute(0, 2, 1, 3)  # (b_s, h, nq, d_k)
-        k = self.fc_k(keys).view(b_s, nk, self.h, self.d_k).permute(0, 2, 3, 1)  # (b_s, h, d_k, nk)
-        v = self.fc_v(values).view(b_s, nk, self.h, self.d_v).permute(0, 2, 1, 3)  # (b_s, h, nk, d_v)
-
-        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
-        if attention_weights is not None:
-            att = att * attention_weights
-        if attention_mask is not None:
-            att = att.masked_fill(attention_mask, -np.inf)
-        att = torch.softmax(att, -1)
-        att=self.dropout(att)
-
-        out = torch.matmul(att, v).permute(0, 2, 1, 3).contiguous().view(b_s, nq, self.h * self.d_v)  # (b_s, nq, h*d_v)
-        out = self.fc_o(out)  # (b_s, nq, d_model)
+        out = torch.bmm(value, attention.permute(0, 2, 1)).view(B, C, H, W)
+        out = self.gamma*out + x
         return out
-    
-class SimSAM(nn.Module):
-    # Simplified SAM by Menghao Guo
-    # "Beyond Self-attention: External Attention using Two Linear Layers for Visual Tasks" arxiv: 2105.02358
-    # Team Guo YYDS!
 
-    def __init__(self, d_model, h,dropout=.1):
-        '''
-        :param d_model: Output dimensionality of the model
-        :param d_k: Dimensionality of queries and keys
-        :param d_v: Dimensionality of values
-        :param h: Number of heads
-        '''
-        super(SimSAM, self).__init__()
+class CAM(nn.Module):
+    '''
+    Channel Attention Module
+    '''
 
-        self.d_model = d_model
-        self.d_k = d_model//h
-        self.d_v = d_model//h
-        self.h = h
+    def __init__(self,in_planes, ratio=8):
+        super(CAM, self).__init__()
+        self.in_planes = in_planes
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
 
-        self.fc_o = nn.Linear(h * self.d_v, d_model)
-        self.dropout=nn.Dropout(dropout)
+    def forward(self, x):
+        """
+            x/out: B*C*H*W
+            q/k/v: B*C*(H*W)
+            attention: B*C*C
+        """
+        B, C, H, W = x.size()
+        query = x.view(B, C, -1)
+        key = x.view(B, C, -1)
+        energy = torch.bmm(query, key.permute(0, 2, 1))
+        attention = self.softmax(torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy)
+        value = x.view(B, C, -1)
 
-
-
-        self.init_weights()
-
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, queries, keys, values, attention_mask=None, attention_weights=None):
-        '''
-        Computes
-        :param queries: Queries (b_s, nq, d_model)
-        :param keys: Keys (b_s, nk, d_model)
-        :param values: Values (b_s, nk, d_model)
-        :param attention_mask: Mask over attention values (b_s, h, nq, nk). True indicates masking.
-        :param attention_weights: Multiplicative weights for attention values (b_s, h, nq, nk).
-        :return:
-        '''
-        b_s, nq = queries.shape[:2]
-        nk = keys.shape[1]
-
-        q = queries.view(b_s, nq, self.h, self.d_k).permute(0, 2, 1, 3)  # (b_s, h, nq, d_k)
-        k = keys.view(b_s, nk, self.h, self.d_k).permute(0, 2, 3, 1)  # (b_s, h, d_k, nk)
-        v = values.view(b_s, nk, self.h, self.d_v).permute(0, 2, 1, 3)  # (b_s, h, nk, d_v)
-
-        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
-        if attention_weights is not None:
-            att = att * attention_weights
-        if attention_mask is not None:
-            att = att.masked_fill(attention_mask, -np.inf)
-        att = torch.softmax(att, -1)
-        att=self.dropout(att)
-
-        out = torch.matmul(att, v).permute(0, 2, 1, 3).contiguous().view(b_s, nq, self.h * self.d_v)  # (b_s, nq, h*d_v)
-        out = self.fc_o(out)  # (b_s, nq, d_model)
+        out = torch.bmm(attention, value).view(B, C, H, W)
+        out = self.gamma*out + x
         return out
     
 class DAM(nn.Module):
     # Dual Attention Module
-    def __init__(self,d_model=512,kernel_size=3,H=7,W=7):
-        super().__init__()
-        self.position_attention_module=PAM(d_model=512,kernel_size=3,H=7,W=7)
-        self.channel_attention_module=CAM(d_model=512,kernel_size=3,H=7,W=7)
+    # 
+    def __init__(self, in_channels, out_channels, ratio=4):
+        super(DAM, self).__init__()
+        self.conv1_1 = nn.Sequential(nn.Conv2d(in_channels, in_channels//ratio, 3, padding=1, bias=False),
+                                    nn.BatchNorm2d(in_channels//ratio),
+                                    nn.ReLU())
+
+        self.conv1_2 = nn.Sequential(nn.Conv2d(in_channels, in_channels//ratio, 3, padding=1, bias=False),
+                                    nn.BatchNorm2d(in_channels//ratio),
+                                    nn.ReLU())
+        
+        self.pa = PAM(in_channels//ratio)
+        self.ca = CAM(in_channels//ratio)
+        self.conv2_1 = nn.Sequential(nn.Conv2d(in_channels//ratio, in_channels//ratio, 3, padding=1, bias=False),
+                                    nn.BatchNorm2d(in_channels//ratio),
+                                    nn.ReLU())
+        self.conv2_2 = nn.Sequential(nn.Conv2d(in_channels//ratio, in_channels//ratio, 3, padding=1, bias=False),
+                                    nn.BatchNorm2d(in_channels//ratio),
+                                    nn.ReLU())
+
+        self.conv3_1 = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(in_channels//ratio, out_channels, 1))
+        self.conv3_2 = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(in_channels//ratio, out_channels, 1))
+        self.conv4 = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(in_channels//ratio, out_channels, 1))
     
-    def forward(self,input):
-        bs,c,h,w=input.shape
-        p_out=self.position_attention_module(input)
-        c_out=self.channel_attention_module(input)
-        p_out=p_out.permute(0,2,1).view(bs,c,h,w)
-        c_out=c_out.view(bs,c,h,w)
-        return p_out+c_out
+    def forward(self, x):
+        feat1 = self.conv1_1(x)
+        pa_feat = self.pa(feat1)
+        pa_conv = self.conv2_1(pa_feat)
+        pa_out = self.conv3_1(pa_conv)
+
+        feat2 = self.conv1_2(x)
+        ca_feat = self.ca(feat2)
+        ca_conv = self.conv2_2(ca_feat) 
+        ca_out = self.conv3_2(ca_conv)
+
+        feat_sum = pa_conv + ca_conv
+
+        feat_sum_out = self.conv4(feat_sum)
+
+        return pa_out, ca_out, feat_sum_out
 
 
 if __name__ == '__main__':
     input=torch.randn(50,512,7,7)
-    danet=DAM(d_model=512,kernel_size=3,H=7,W=7)
-    print(danet(input).shape)
+    danet=DAM(512,10)
+    print(danet(input)[0].shape,danet(input)[1].shape,danet(input)[2].shape)
     print('okk!')
--- models/siamunet_diff.py
+++ models/siamunet_diff.py
@@ -38,12 +38,16 @@ class SiamUnet_diff(nn.Module):
             encoder_layer=[]
             conv1 = self.conv(self.in_channels, self.num_channels[i]) if i == 0 else \
                 self.conv(self.num_channels[i-1], self.num_channels[i])
+            bn1 = self.batchnorm(self.num_channels[i])
             conv2 = self.conv(self.num_channels[i], self.num_channels[i])
-            bn = self.batchnorm(self.num_channels[i])
+            bn2 = self.batchnorm(self.num_channels[i])
             relu = nn.ReLU()
             dropout = nn.Dropout2d(p=0.2)
-            encoder_layer.extend([conv1,bn,relu,dropout,conv2,bn,relu,dropout])
-            if i >= 2: encoder_layer.extend([conv2,bn,relu,dropout])
+            encoder_layer.extend([conv1,bn1,relu,dropout,conv2,bn2,relu,dropout])
+            if i >= 2: 
+                conv3 = self.conv(self.num_channels[i], self.num_channels[i])
+                bn3 = self.batchnorm(self.num_channels[i])
+                encoder_layer.extend([conv3,bn3,relu,dropout])
             encoder_layers.append(nn.Sequential(*encoder_layer))
         return encoder_layers
     
@@ -52,15 +56,17 @@ class SiamUnet_diff(nn.Module):
         for i in range(4):
             decoder_layer=[]
             deconv1 = self.deconv(self.num_channels[i+1], self.num_channels[i])
-            if i >= 2: deconv2 = self.conv(self.num_channels[i], self.num_channels[i])
+            bn1 = self.batchnorm(self.num_channels[i])
             deconv3 = self.conv(self.num_channels[i], self.out_channels) if i == 0 else \
                 self.conv(self.num_channels[i], self.num_channels[i-1])
-            bn1 = self.batchnorm(self.num_channels[i])
-            bn2 = self.batchnorm(self.out_channels) if i == 0 else self.batchnorm(self.num_channels[i-1])
+            bn3 = self.batchnorm(self.out_channels) if i == 0 else self.batchnorm(self.num_channels[i-1])
             relu = nn.ReLU()
             dropout = nn.Dropout2d(p=0.2)
-            if i >= 2: decoder_layer.extend([deconv1,bn1,relu,dropout,deconv2,bn1,relu,dropout,deconv3,bn2,relu,dropout])
-            else: decoder_layer.extend([deconv1,bn1,relu,dropout,deconv3,bn2,relu,dropout])
+            if i >= 2: 
+                deconv2 = self.conv(self.num_channels[i], self.num_channels[i])
+                bn2 = self.batchnorm(self.num_channels[i])
+                decoder_layer.extend([deconv1,bn1,relu,dropout,deconv2,bn2,relu,dropout,deconv3,bn3,relu,dropout])
+            else: decoder_layer.extend([deconv1,bn1,relu,dropout,deconv3,bn3,relu,dropout])
             decoder_layers.append(nn.Sequential(*decoder_layer))
         return decoder_layers
 
